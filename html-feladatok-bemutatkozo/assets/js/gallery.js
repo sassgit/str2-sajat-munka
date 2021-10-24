757a759e47dcb5b993dbcacab6a82d63a05492c3
sassi <attila.sassi@gmail.com>
2021-10-25 00:45:03 +0200

    css-feladatok-admin

--- html-feladatok-bemutatkozo/assets/js/gallery.js
+++ html-feladatok-bemutatkozo/assets/js/gallery.js
@@ -1,4 +1,4 @@
 import { loadImages } from "./lib.js";
 
-loadImages(document.querySelector("#image-gallery"), "/assets/img/gallery/index.txt", "/assets/img/gallery/", ["img-container", "gallery-image-container"], ["work-image","gallery-image"] );
+loadImages(document.querySelector("#image-gallery"), "./assets/img/gallery/index.txt", "./assets/img/gallery/", ["img-container", "gallery-image-container"], ["work-image","gallery-image"] );
 
