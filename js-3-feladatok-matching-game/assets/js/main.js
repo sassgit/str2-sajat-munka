9194e54a017e7e39d69ae2f2d2362720654907c9
sassi <attila.sassi@gmail.com>
2021-11-14 00:25:01 +0100

    match test4

--- js-3-feladatok-matching-game/assets/js/main.js
+++ js-3-feladatok-matching-game/assets/js/main.js
@@ -1,38 +1,106 @@
+'use strict';
+import {
+  createElementTree,
+  appendChildren
+} from "./objectElement.js";
+
 const gameDiv = document.querySelector("#game");
-    let cardCount = 100;
-    const cards = [];
-    const cardImg = [];
-    const createDiv = (...classnames) => {
-      const div = document.createElement('div');
-      if (classnames.length)
-      div.classList.add(...classnames);
-      return div;
-    }
-    
-
-
-    const createCards = (count) => {
-    for (let i = 0;i<count;i++) {
-      const card = createDiv('card');
-      const cardInner = createDiv('card-inner');
-      const cardFront = createDiv('card-front');
-      const imageWrapper = createDiv('image-wrapper');
-      const img = document.createElement('img');
-      const cardBack = createDiv('card-back');
-      img.src = 'assets/cardimages/image001.png';
-      imageWrapper.appendChild(img);
-      cardFront.appendChild(imageWrapper);
-      cardInner.appendChild(cardFront);
-      cardInner.appendChild(cardBack);
-      card.appendChild(cardInner);
-      gameDiv.appendChild(card);
-      cards.push(card);
-      cardImg.push(img);
+const timeElement = document.querySelector('h2');
+const cardTree = {
+  '.card': {
+    '.card-inner': {
+      '.card-front': {
+        '.image-wrapper': {
+          'img.card-image': ''
+        }
+      },
+      '.card-back': '',
     }
   }
+};
+const cards = [];
+const cardImg = [];
+const imageCount = 53;
 
-  const init = () => {
-    createCards(cardCount);
+/**
+ * Maximális értéke Math.floor(imageCount / 2), vagyis 104
+ */
+let cardCount = 10;
+
+let timestart = 0;
+let timeHandler = 0;
+
+const pad00 = (n) => n.toString().padStart(2, '0');
+
+/**
+ * 
+ * @param {zero based index} idx
+ * @returns url of idx-th image
+ */
+
+const getImageUrl = (idx) => `assets/cardimages/image0${pad00(idx + 1)}.png`;
+
+const printTime = () => {
+  const sec = Math.floor((Date.now() - timestart) / 1000);
+  timeElement.textContent = `${pad00(Math.floor(sec / 60))}:${ pad00(sec % 60)}`;
+};
+
+const initTime = () => {
+  timestart = Date.now();
+  printTime();
+  timeHandler = setInterval(printTime, 1000);
+
+}
+
+const cardClick = ev => {
+
+  let card = ev.target;
+  if (!card.classList.contains('card-flipped')) {
+    if (!timestart) initTime();
   }
 
-  init();
+  if (card.classList.contains('card-flipped'))
+  card.classList.remove('card-flipped');
+  else
+  card.classList.add('card-flipped');
+  const checkArr = [...Array(cardCount / 2)].fill(0);
+  cards.forEach((e, i) => {
+    if (e.classList.contains('card-flipped'))
+    checkArr[cardImg[i].alt]++;
+  });
+  cardImg.forEach((e, i) => {
+    if (checkArr[e.alt] == 2)
+      cards[i].classList.add('card-disabled');
+  });
+}
+
+const createCards = (count) => {
+  gameDiv.innerHTML = '';
+  cards.length = 0;
+  cardImg.length = 0;
+  appendChildren(gameDiv, ...createElementTree(Array(count).fill(cardTree)));
+  cards.push(...gameDiv.querySelectorAll('.card'));
+  cardImg.push(...gameDiv.querySelectorAll('.card-image'));
+  cards.forEach(e => e.addEventListener('click', cardClick));
+}
+
+const randomSort = (length) => [...Array(length)].map((e, i) => [i, Math.random()]).sort((a, b) => a[1] - b[1]).map(e => e[0]);
+
+const initImages = () => {
+  const imgRand = randomSort(imageCount);
+  const cardRand = randomSort(cardCount);
+  document.documentElement.style.setProperty('--back-picture', `url('../../${getImageUrl(imgRand[cardCount / 2])}'`);
+  // document.documentElement.style.setProperty('--back-color', `hsla(${Math.floor(Math.random() * 360)}, 50%, 35%, .75)`);
+  cardImg.forEach((e, i) => {
+    const idx = Math.floor(cardRand[i] / 2);
+    e.src = getImageUrl(imgRand[idx]);
+    e.alt = idx;
+  });
+}
+
+const init = () => {
+  if (cardCount != cards.length) createCards(cardCount);
+  initImages();
+}
+
+init();
---
+++ js-3-feladatok-matching-game/assets/js/objectElement.js
@@ -0,0 +1,47 @@
+const createElementWithClasses = (tag, ...classnames) => {
+  const elem = document.createElement(tag);
+  if (classnames.length)
+    elem.classList.add(...classnames);
+  return elem;
+}
+
+const createElementFromString = str => {
+  if (!str)
+    return null;
+  else {
+    const arr = str.split('.');
+    return createElementWithClasses(arr.shift() || 'div', arr);
+  }
+}
+
+const appendChildren = (element, ...children) => children.forEach(e => e ? element.appendChild(e) : 0);
+
+const createElementTree = (treeObj) => {
+  const retElements = [];
+  if (treeObj) {
+    if (typeof treeObj == 'string')
+      retElements.push(createElementFromString(treeObj));
+    else if (typeof treeObj == 'object') {
+      if (Array.isArray(treeObj))
+        treeObj.forEach(e => retElements.push(...createElementTree(e)));
+      else
+        for (let e in treeObj) {
+          const elem = createElementFromString(e);
+          const inner = treeObj[e]
+          if (typeof inner != 'object' && typeof inner != 'undefined')
+            elem.textContent = inner.toString();
+          else
+            appendChildren(elem, ...createElementTree(treeObj[e]));
+          retElements.push(elem);
+        }
+      }
+    }
+    return retElements;
+}
+
+export {
+  createElementTree,
+  appendChildren,
+  createElementFromString,
+  createElementWithClasses,
+};
